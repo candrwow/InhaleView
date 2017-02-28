3bd684152c832317e824ee0426514bd4440ea4a2
Candrwow <Candrwow@gmail.com>
2017-02-28 14:42:48 +0800

    Initial commit

---
+++ app/src/main/java/candrwow/inhaleview/MainActivity.java
@@ -0,0 +1,13 @@
+package candrwow.inhaleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
